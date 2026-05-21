057ed0c50bd1d7d5eea208114fa438d5a56525ca
github-action-benchmark <github@users.noreply.github.com>
2026-05-21 22:51:05 +0000

    add Soteria benchmarks (customSmallerIsBetter) benchmark result for 00082a38448e2f51abf0f4685e36fad06d69a425

---
+++ dev/bench/data.js
@@ -0,0 +1,67 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1779403865499,
+  "repoUrl": "https://github.com/giltho/soteria",
+  "entries": {
+    "Soteria benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "sachaayoun@gmail.com",
+            "name": "Sacha Ayoun",
+            "username": "giltho"
+          },
+          "committer": {
+            "email": "sachaayoun@gmail.com",
+            "name": "Sacha Ayoun",
+            "username": "giltho"
+          },
+          "distinct": true,
+          "id": "00082a38448e2f51abf0f4685e36fad06d69a425",
+          "message": "Merge branch 'benchmarks'",
+          "timestamp": "2026-05-22T00:44:42+02:00",
+          "tree_id": "5ccd302ce3f2942e1b742017167e2c95bd2aea40",
+          "url": "https://github.com/giltho/soteria/commit/00082a38448e2f51abf0f4685e36fad06d69a425"
+        },
+        "date": 1779403863704,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "rust-file: char_min_max",
+            "value": 0.07796946734000001,
+            "range": "± 0.0015",
+            "unit": "s"
+          },
+          {
+            "name": "rust-file: kani-any",
+            "value": 0.09535803986000001,
+            "range": "± 0.0011",
+            "unit": "s"
+          },
+          {
+            "name": "rust-crate: cargo.t",
+            "value": 0.0834487737,
+            "range": "± 0.0021",
+            "unit": "s"
+          },
+          {
+            "name": "c: array",
+            "value": 0.0310826698,
+            "range": "± 0.0009",
+            "unit": "s"
+          },
+          {
+            "name": "c: duffs",
+            "value": 0.03133867436,
+            "range": "± 0.0005",
+            "unit": "s"
+          },
+          {
+            "name": "c-gen-summaries: simple.t-gen-summaries",
+            "value": 0.23273844600771554,
+            "unit": "s"
+          }
+        ]
+      }
+    ]
+  }
+}
